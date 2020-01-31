b1b2cc335ba5457c64602b64a1bbd7c119d456cb
Matthew Bell <matthewbell1030@gmail.com>
2020-01-30 20:55:10 -0800

    updated logic.js

--- assets/logic/logic.js
+++ assets/logic/logic.js
@@ -4,7 +4,13 @@ console.log("logic.js is successfully linked");
 
 
 
+//set parameter for search term
 
+//set parameter for number of records to retrieve
+
+//set parameter for start year (optional)
+
+//set parameter for end year (optional)
 
 
 
