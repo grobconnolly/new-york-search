3de07c869eb562d990fd56d0b352172eb4b51160
Julian Acosta <8892386+julacosta9@users.noreply.github.com>
2020-01-30 20:53:44 -0800

    declare placeholder variables and start building api request URL

--- assets/logic/logic.js
+++ assets/logic/logic.js
@@ -1,7 +1,26 @@
 $(document).ready(function() {
 console.log("logic.js is successfully linked");
 
+//set parameter for search term
+let query;
 
+//set parameter for number of records to retrieve
+let numOfRecords;
+
+//set parameter for start year (optional)
+let startYear = "sy=" + valueoffield;
+
+//set parameter for end year (optional)
+let endYear;
+
+let queryURL = "https://api.nytimes.com/svc/search/v2/articlesearch.json?q=" + query +  "api-key=frybrVidz4oQ4nanro4Ft8CKfvGkIni3"
+
+    $.ajax({
+        url: "https://api.nytimes.com/svc/search/v2/articlesearch.json?q=election&api-key=frybrVidz4oQ4nanro4Ft8CKfvGkIni3"
+        method: "GET"
+    }).then(function(repsonse) {
+        console.log(response);
+    })
 
 
 
