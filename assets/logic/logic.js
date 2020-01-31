5f24b67f0951e84eb1c8d4b04ee7aaac829ade06
Matthew Bell <matthewbell1030@gmail.com>
2020-01-30 21:01:03 -0800

    committing for loop and results variable to logic.js

--- assets/logic/logic.js
+++ assets/logic/logic.js
@@ -2,16 +2,16 @@ $(document).ready(function() {
 console.log("logic.js is successfully linked");
 
 //set parameter for search term
-let query;
+let query = "";
 
 //set parameter for number of records to retrieve
-let numOfRecords;
+let numOfRecords = "";
 
 //set parameter for start year (optional)
 let startYear = "sy=" + valueoffield;
 
 //set parameter for end year (optional)
-let endYear;
+let endYear = "";
 
 let queryURL = "https://api.nytimes.com/svc/search/v2/articlesearch.json?q=" + query +  "api-key=frybrVidz4oQ4nanro4Ft8CKfvGkIni3";
 
@@ -21,6 +21,12 @@ let queryURL = "https://api.nytimes.com/svc/search/v2/articlesearch.json?q=" + q
     }).then(function(response) {
         console.log(response);
     });
+
+    var results = response.data;
+
+    for (var i = 0; i < results.length; i++) {
+
+    }
 
 
 //set parameter for search term
